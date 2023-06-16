37ea0ee2ae2e3fc8273de8b9202cd4dc68e86c62
Yuxi Long <longyuxi@live.com>
2023-06-16 10:43:28 -0400

    gbr rerun scripts

---
+++ svm/gbr_reruns/dispatch_jobs.py
@@ -0,0 +1,194 @@
+import glob
+import os
+import pandas as pd
+import pathlib
+import logging
+import redis
+import numpy as np
+from Bio.PDB import PDBParser, PDBIO
+from tqdm import tqdm
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+###############################
+# Platform specific variables #
+#                             #
+# Change to fit to job        #
+###############################
+
+
+KEY_PREFIX = 'gbr_rerun_50_50' # Prefix of every job, as appears in the Redis database
+TRAIN_RATIO = 0.5
+CLUSTER = 'CS' # or 'DCC'
+
+if CLUSTER == 'CS':
+    cwd = pathlib.Path(__file__).parent.resolve()
+    NUM_JOBS_TO_SUBMIT = 100
+    PYTHON_EXECUTABLE = '/usr/project/dlab/Users/jaden/mambaforge/envs/tnet2017/bin/python'
+    ROOT_DIR = '/usr/project/dlab/Users/jaden/tnet2017-new/svm/gbr_reruns'
+    os.system(f'mkdir -p {ROOT_DIR}/slurm-outs')
+    SBATCH_TEMPLATE = f"""#!/bin/zsh
+#SBATCH --requeue
+#SBATCH --chdir={ROOT_DIR}
+#SBATCH --output={ROOT_DIR}/slurm-outs/%x-%j-slurm.out
+#SBATCH --mem=12000M
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=grisman
+#SBATCH --exclude=jerry1,jerry2
+
+source ~/.zshrc
+date
+hostname
+conda activate algtop-ph
+cd {ROOT_DIR}
+
+
+    """
+
+    DB = redis.Redis(host='cybermen', port=6379, decode_responses=True, password="topology")
+
+    ADDITIONAL_SAVE_FOLDER = ROOT_DIR + '/additional_results'
+    os.system(f'mkdir -p {ADDITIONAL_SAVE_FOLDER}')
+
+elif CLUSTER == 'DCC':
+    raise NotImplementedError
+    NUM_JOBS_TO_SUBMIT = 81000
+    PYTHON_EXECUTABLE = '/hpc/group/donald/yl708/mambaforge/envs/tnet2017/bin/python'
+    ROOT_DIR = '/hpc/group/donald/yl708/TopologyNet-2017/perturbations'
+    SBATCH_TEMPLATE = f"""#!/bin/bash
+#SBATCH --partition=common-old,scavenger
+#SBATCH --requeue
+#SBATCH --chdir={ROOT_DIR}
+#SBATCH --output={ROOT_DIR}/slurm_logs/%x-%j-slurm.out
+#SBATCH --mem=2500M
+
+source ~/.bashrc
+source ~/.bash_profile
+date
+hostname
+conda activate tnet2017
+cd {ROOT_DIR}
+
+
+    """
+
+    DB = redis.Redis(host='dcc-login-03', port=6379, decode_responses=True, password="topology")
+    ORIGINAL_PROTEIN_FILE = '/hpc/group/donald/yl708/perturbations-data/1a4k_protein.pdb'
+    LIGAND_FILE = '/hpc/group/donald/yl708/perturbations-data/1a4k_ligand.mol2'
+    PERTURBATION_SAVE_FOLDER = '/hpc/group/donald/yl708/perturbations-data/'
+
+else:
+    raise Exception     # Incorrect specification of cluster variable
+
+
+
+#############################
+# Pre-execution Tests       #
+#############################
+
+# Database connection
+DB.set('connection-test', '123')
+if DB.get('connection-test') == '123':
+    DB.delete('abc')
+    logging.info('Database connection successful')
+else:
+    raise Exception     # Database connection failed
+
+
+#############################
+# Actual logic              #
+#############################
+
+# Each entry in the redis database should contain the additional information
+
+# Run index
+# Run seed
+# R^2
+# MSE
+# Pearson
+# Predictions (filename)
+# Scatter plot (filename)
+
+
+def main(dry_run=False, rebuild_all_keys=False):
+    # Initialize database on first run
+    if dry_run:
+        populate_db(rebuild_all_keys=rebuild_all_keys)
+
+    # Then submit jobs until either running out of entries or running out of number of jobs to submit
+    i = 0
+
+    database_keys = DB.keys(KEY_PREFIX + '*')
+    for key in database_keys:
+        if i == NUM_JOBS_TO_SUBMIT:
+            break
+        info = DB.hgetall(key)
+
+        if info['finished'] == 'True' and info['error'] == 'False':
+        # if info['attempted'] == 'True':
+            continue
+        else:
+            i += 1
+            # submit job for it
+            if not dry_run:
+                info['attempted'] = 'True'
+                DB.hset(key, mapping=info)
+
+                # sbatch run job wrapper
+                sbatch_cmd = SBATCH_TEMPLATE + f'\n{PYTHON_EXECUTABLE} {str(pathlib.Path(__file__).parent) + "/job_wrapper.py"} --key {key}'
+
+                # print(sbatch_cmd)
+                with open('run.sh', 'w') as f:
+                    f.write(sbatch_cmd)
+
+                os.system(f'sbatch --job-name={key} run.sh')
+
+    if dry_run:
+        print(f'Number of jobs that would be submitted: {i}')
+        time.sleep(5)
+    else:
+        print(f'Number of jobs submitted: {i}')
+
+
+def populate_db(rebuild_all_keys=False):
+    logging.info('Populating database')
+    n_jobs = NUM_JOBS_TO_SUBMIT
+    keys = [KEY_PREFIX + str(i) for i in range(n_jobs)]
+
+    database_keys = DB.keys()
+    for k in tqdm(keys):
+        if not rebuild_all_keys and k in database_keys:
+            logging.debug(f"Key {k} already exists in database")
+            continue
+
+        if rebuild_all_keys:
+            DB.delete(k)
+
+        # Add the entry to the database
+        DB.hset(k, mapping={
+            'attempted': 'False',
+            'error': 'False',
+            'finished': 'False',
+            'seed': f'{hash(k)}',
+            'train_ratio': f'{TRAIN_RATIO}',
+            'prediction_results_file': f'{ADDITIONAL_SAVE_FOLDER}/{k}_predictions.npy',
+            'scatter_plot_file': f'{ADDITIONAL_SAVE_FOLDER}/{k}_scatter_plot.html',
+            'r2': '0',
+            'rmse': '0',
+            'pearson': '0',
+        })
+
+
+
+def rebuild_db():
+    raise NotImplementedError
+
+def get_db():
+    # Pinnacle of OOP
+    return DB
+
+if __name__ == '__main__':
+    # rebuild_db()
+    main(dry_run=True, rebuild_all_keys=True)
+    main(dry_run=False)
---
+++ svm/gbr_reruns/job_wrapper.py
@@ -0,0 +1,123 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent / '..'))
+
+import argparse
+import traceback
+import pickle
+from gtda.diagrams import PersistenceImage
+import numpy as np
+
+import dispatch_jobs
+import svm_ph
+
+DB = dispatch_jobs.get_db()
+pim = PersistenceImage()
+
+def job(key):
+    # The part where the job actually runs, given df and idx as input
+
+    # Each entry in the redis database should be a dictionary in the following form
+    # {'error': 'False', 'finished': 'False', 'pearson': '0', 'rmse': '0', 'seed': '-3981833901435230303', 'prediction_results_file': '/usr/project/dlab/Users/jaden/tnet2017-new/svm/gbr_reruns/additional_results/gbr_rerun_90_10_0_predictions.npy', 'r2': '0', 'train_ratio': '0.9', 'scatter_plot_file': '/usr/project/dlab/Users/jaden/tnet2017-new/svm/gbr_reruns/additional_results/gbr_rerun_90_10_0_scatter_plot.html', 'attempted': 'True'}
+
+    # Need to set r2, rmse, pearson
+
+
+    d = DB.hgetall(key)
+
+    seed = int(d['seed']) % (2**32)
+    train_ratio = float(d['train_ratio'])
+
+
+
+    import numpy as np
+    from pathlib import Path
+    import pickle
+
+    # Loading the files back in
+    observations = []
+    binding_affinities = []
+
+    NPYS_FOLDER = Path('/usr/project/dlab/Users/jaden/tnet2017-new/svm/npys')
+
+    for file_index in range(len(list(NPYS_FOLDER.glob('binding_affinities_*.npy')))):
+        with open(NPYS_FOLDER / f'observations_{file_index}.npy', 'rb') as f:
+            observations.append(np.load(f))
+
+        with open(NPYS_FOLDER / f'binding_affinities_{file_index}.npy', 'rb') as f:
+            binding_affinities.append(np.load(f))
+
+    observations = np.concatenate(observations)
+    binding_affinities = np.concatenate(binding_affinities)
+
+    print(observations.shape)
+    print(binding_affinities.shape)
+
+
+    from sklearn.model_selection import train_test_split
+
+    observations = observations.reshape(observations.shape[0], -1)  # Flatten out for SVM
+
+    X_train, X_test, y_train, y_test = train_test_split(observations, binding_affinities, train_size=train_ratio, random_state=seed)
+
+    from sklearn.ensemble import GradientBoostingRegressor
+
+    regr = GradientBoostingRegressor()
+    print('Starting fit')
+
+    regr.fit(X_train, y_train)
+
+    import plotly.express as px
+    from sklearn.metrics import r2_score
+
+    print('Starting predict')
+
+    y_hat = regr.predict(X_test)
+
+    fig = px.scatter(x=y_test, y=y_hat, labels={'x': 'True binding affinity', 'y': 'Predicted binding affinity'}, title=f'True vs Predicted binding affinity\nR^2 = {r2_score(y_test, y_hat):.2f}, n = {len(y_test)}, RMSE: {np.sqrt(np.mean((y_test - y_hat)**2)):.2f}, Pearson: {np.corrcoef(y_test, y_hat)[0, 1]:.2f}')
+    fig.show()
+
+    fig.write_html(d['scatter_plot_file'])
+
+    np.save(d['prediction_results_file'], np.stack((y_test, y_hat)))
+
+    print(f'RMSE: {np.sqrt(np.mean((y_test - y_hat) ** 2)): .4f}')
+    print(f'Pearson correlation: {np.corrcoef(y_test, y_hat)[0, 1]: .4f}')
+
+    d['r2'] = f'{r2_score(y_test, y_hat):.4f}'
+    d['rmse'] = f'{np.sqrt(np.mean((y_test - y_hat) ** 2)):.4f}'
+    d['pearson'] = f'{np.corrcoef(y_test, y_hat)[0, 1]:.4f}'
+    DB.hset(key, mapping=d)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key')
+
+    args = parser.parse_args()
+    key = args.key
+
+    print('key', key)
+    print('job started')
+    d = DB.hgetall(key)
+    d['attempted'] = 'True'
+    DB.hset(key, mapping=d)
+
+    try:
+        job(key)
+        print('job finished')
+        d = DB.hgetall(key)
+        d['finished'] = 'True'
+        d['error'] = 'False'
+        DB.hset(key, mapping=d)
+        print('job success')
+
+    except Exception as err:
+        print(Exception, err)
+        print(traceback.format_exc())
+        print('job error')
+
+        d = DB.hgetall(key)
+        d['finished'] = 'True'
+        d['error'] = 'True'
+        DB.hset(key, mapping=d)
