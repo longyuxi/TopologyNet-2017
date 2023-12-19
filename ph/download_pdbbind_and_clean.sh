414e171add00c6fc869f1efd0c6602033a406813
Yuxi Long <longyuxi@live.com>
2023-12-19 10:56:53 -0500

    removed trailing whitespace from header

--- ph/download_pdbbind_and_clean.sh
+++ ph/download_pdbbind_and_clean.sh
@@ -1,20 +1,20 @@
 ## This file is part of PATH, which is part of OSPREY 3.0
-## 
+##
 ## OSPREY Protein Redesign Software Version 3.0
 ## Copyright (C) 2001-2023 Bruce Donald Lab, Duke University
-## 
+##
 ## OSPREY is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License version 2
 ## as published by the Free Software Foundation.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with OSPREY.  If not, see <http://www.gnu.org/licenses/>.
-## 
+##
 ## OSPREY relies on grants for its development, and since visibility
 ## in the scientific literature is essential for our success, we
 ## ask that users of OSPREY cite our papers. See the CITING_OSPREY
 ## document in this distribution for more information.
-## 
+##
 ## Contact Info:
 ##    Bruce Donald
 ##    Duke University
@@ -24,7 +24,7 @@
 ##    NC 27708-0129
 ##    USA
 ##    e-mail: www.cs.duke.edu/brd/
-## 
+##
 ## <signature of Bruce Donald>, Mar 1, 2023
 ## Bruce Donald, Professor of Computer Science
 
